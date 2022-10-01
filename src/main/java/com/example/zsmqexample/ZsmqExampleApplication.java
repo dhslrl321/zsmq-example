9b380a144ef4413cd8be85cab5495132819fef41
dhslrl321 <dhslrl321@gmail.com>
2022-10-01 21:49:47 +0900

    commit

---
+++ src/main/java/com/example/zsmqexample/ZsmqExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.zsmqexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZsmqExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZsmqExampleApplication.class, args);
+    }
+
+}
